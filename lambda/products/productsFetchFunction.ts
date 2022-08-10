6981f13306d3379da4afdb8a01e7d649d8c84e6a
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-08-10 18:53:21 -0300

    add: código da função lambda de pesquisa de produtos

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,48 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(
+    // Método que será invocado quando a função Lambda for invocada.
+    event: APIGatewayProxyEvent,
+    // event é um objeto que representa o evento que disparou a função Lambda.
+    // Neste caso, o evento é uma requisição REST HTTP feita para a API Gateway.
+    // O objeto event contém informações sobre a requisição, como o método HTTP
+    // utilizado, o corpo da requisição, os headers, etc.
+
+    context: Context
+    // context é um objeto que contém informações sobre a execução da função
+    // Lambda, como o nome da função, o nome do log group, o nome do log
+    // stream, etc. É injetado pelo próprio AWS Lambda.
+
+): Promise<APIGatewayProxyResult>
+// Promise é uma classe que representa uma operação assíncrona. O método
+// handler é assíncrono porque ele acessa o banco de dados DynamoDB para
+// buscar os produtos. O método handler retorna uma Promise porque a
+// função Lambda deve retornar uma Promise.
+
+// APIGatewayProxyResult é um objeto que representa a resposta HTTP que
+// será enviada para o cliente que fez a requisição. O objeto contém
+// informações sobre o status code da resposta, o body da resposta, os
+// headers da resposta, etc.
+
+{
+    const method = event.httpMethod;
+    if (event.resource === "/products") {
+        if (method === "GET") {
+            console.log("GET /products");
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "GET /products",
+                }),
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: "Bad Request",
+        }),
+    }
+}
