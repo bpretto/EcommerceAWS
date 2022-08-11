8e6643ef8a861ba875ee2f73a6f1a21c38d021bd
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-08-11 14:44:54 -0300

    add: integração da api gateway com a ProductsFetchFunction

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -4,6 +4,16 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwlogs from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+    // Interface que representa as propriedades a serem passadas para
+    // a stack do CDK. Essa interface é opcional, mas é uma boa prática
+    // criar uma interface para as propriedades da stack, pois facilita
+    // a manutenção do código e a reutilização de código.
+
+    productsFetchHandler: lambdaNodejs.NodejsFunction;
+    // Atributo que representa a função Lambda Products Fetch.
+};
+
 export class EcommerceApiStack extends cdk.Stack {
     // Esta classe representa a stack do CDK que contém a API Gateway.
     // A API Gateway é o ponto de entrada da aplicação. Ela recebe as
@@ -13,7 +23,7 @@ export class EcommerceApiStack extends cdk.Stack {
     // Não é necessário criar um atributo de classe para a API Gateway
     // porque ela não será lida por nenhuma outra stack.
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
         super(scope, id, props);
 
         const api = new apigateway.RestApi(
@@ -25,5 +35,24 @@ export class EcommerceApiStack extends cdk.Stack {
                 // do API Gateway.
             }
         );
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(
+            props.productsFetchHandler,
+            // Função Lambda que será invocada quando a API Gateway receber
+            // uma requisição HTTP para o endpoint /products.
+        );
+        // Cria um objeto que representa a integração da API Gateway com
+        // a função Lambda Products Fetch. A integração é responsável por
+        // invocar a função Lambda quando a API Gateway receber uma
+        // requisição HTTP para o endpoint /products e também por converter
+        // a resposta da função Lambda em uma resposta HTTP.
+
+        const productsResource = api.root.addResource("products");
+        // "/products"
+
+        productsResource.addMethod("GET", productsFetchIntegration);
+        // "/products" GET
+        // GET em /products -> productsFetchIntegration -> productsFetchHandler
+
     }
 }
