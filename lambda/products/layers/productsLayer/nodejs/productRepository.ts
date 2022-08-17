a1c0d4b5374a36a76563e4e2b0cd51bfedbbef07
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-08-17 19:24:59 -0300

    add: busca de todos os produtos no DynamoDB

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,4 +1,6 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+
 import { v4 as uuid } from "uuid";
 
 export interface Product {
@@ -20,8 +22,17 @@ export class ProductRepository {
     private productsDdb: string;
 
     constructor(ddbClient: DocumentClient, productsDdb: string) {
-        this.ddbClient = ddbClient;
-        this.productsDdb = productsDdb;
+        this.ddbClient = ddbClient; // Cliente que acessa o DynamoDB.
+        this.productsDdb = productsDdb; // Nome da tabela do DynamoDB.
     }
+
+    async getAllProducts(): Promise<Product[]> {
+        const data = await this.ddbClient.scan({
+            TableName: this.productsDdb,
+        }).promise();
+        return data.Items as Product[];
+    };
+    // Operação com fins didáticos, o ideal não é buscar todos
+    // os produtos, sem nenhum tipo de filtro.
 
 }
